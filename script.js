1b4d079102ab74f447605bafd29d71814daccab1
Anastasia <nastya.lyugaeva@bk.ru>
2020-12-31 01:00:20 +0300

    fix popup, feat new function

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let likes = document.querySelectorAll('.element__like');
 let openButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.overlay__button');
 let overlay = document.querySelector('.overlay');
@@ -9,36 +8,41 @@ let newName = overlay.querySelector('.overlay__input_form_name');
 let profileSub = document.querySelector('.profile__subscription');
 let newSub = overlay.querySelector('.overlay__input_form_subscription');
 
+
+function openPopup(){
+  overlay.classList.add('overlay_active');
+  newName.value = profileName.textContent;
+  newSub.value = profileSub.textContent;
+}
+
+function closePopup() {
+  overlay.classList.remove('overlay_active');
+}
+
+function submit(event){
+  event.preventDefault();
+  profileName.textContent = newName.value;
+  profileSub.textContent = newSub.value;
+  closePopup();
+}
+
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+form.addEventListener('submit', submit);
+
+/*Возможно, в будущем придется удалить */
+overlay.addEventListener('click', function(event){
+  if (event.target === event.currentTarget){
+    closePopup();
+  }
+  });
+
+/* Реализаци  работы лайков
+let likes = document.querySelectorAll('.element__like');
 for (let i=0; i<likes.length; i++){
   likes[i].addEventListener('click',function(){
     likes[i].classList.toggle('element__like_black');
   }
     )
-};
-
-function togglePopup() {
-  overlay.classList.toggle('overlay_active');
-}
-
-openButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-buttonSubmit.addEventListener('click',togglePopup);
-
-
-overlay.addEventListener('click', function(event){
-if (event.target === event.currentTarget){
-  togglePopup();
-}
-})
-
-buttonSubmit.addEventListener('click', ()=>{
-profileName.textContent = newName.value;
-profileSub.textContent = newSub.value;
-})
-
-form.addEventListener('submit', event => {
-  event.preventDefault();
-});
-
-
+};*/
 
