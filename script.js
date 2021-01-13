034a9b919aa2c0e0d4841d392fb5224f23e23cf0
Anastasia <nastya.lyugaeva@bk.ru>
2021-01-13 23:36:47 +0300

    feat delete bin

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ let profileName = document.querySelector('.profile__name')
 let newName = overlay.querySelector('.overlay__input_form_name');
 let profileSub = document.querySelector('.profile__subscription');
 let newSub = overlay.querySelector('.overlay__input_form_subscription');
-const likes = document.querySelectorAll('.element__like');
+const likes = document.querySelectorAll('.element__like'),
+      addButton = document.querySelector('.profile__add-button'),
+      popupEdit = document.querySelector('.overlay__popup_edit-form'),
+      popupAdd = document.querySelector('.overlay__popup_add-form'),
+      bin = document.querySelectorAll('.element__bin');
 
 
 
@@ -32,13 +36,14 @@ openButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 form.addEventListener('submit', submit);
 
-/*Возможно, в будущем придется удалить */
+//Возможно, в будущем придется удалить
 overlay.addEventListener('click', function(event){
   if (event.target === event.currentTarget){
     closePopup();
   }
   });
 
+// лайки
 
 likes.forEach(item => {
   item.addEventListener('click', function(){
@@ -47,6 +52,14 @@ likes.forEach(item => {
 })
 
 
+//удаение элементов
+
+bin.forEach(item => {
+item.addEventListener('click', function(){
+item.parentElement.remove();
+})
+})
+
 /*let likes = document.querySelectorAll('.element__like');
 for (let i=0; i<likes.length; i++){
   likes[i].addEventListener('click',function(){
@@ -54,4 +67,3 @@ for (let i=0; i<likes.length; i++){
   }
     )
 };*/
-
