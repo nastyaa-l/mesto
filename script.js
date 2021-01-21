8bf3c1242c9defebf19c210ed0a997b2ce5cbf63
Anastasia <nastya.lyugaeva@bk.ru>
2021-01-21 03:28:18 +0300

    refactor addElements

--- script.js
+++ script.js
@@ -2,40 +2,35 @@
 const openButton = document.querySelector('.profile__edit-button'),
       closeButtonEdit = document.querySelector('.overlay__button_form-edit'),
       closeButtonAdd = document.querySelector('.overlay__button_form-add'),
-      overlay = document.querySelector('.overlay'),
-      form = overlay.querySelector('.overlay__form'),
-      buttonSubmit = overlay.querySelector('.overlay__submit'),
-      likes = document.querySelectorAll('.element__like'),
+      form = document.querySelector('.overlay__form'),
       addButton = document.querySelector('.profile__add-button'),
-      popupEdit = document.querySelector('.overlay__popup_edit-form'),
-      popupAdd = document.querySelector('.overlay__popup_add-form'),
-      bin = document.querySelectorAll('.element__bin'),
-      editPopup = document.querySelector('.overlay__popup_form-edit'),
-      addForm = document.querySelector('.overlay__submit_form-add'),
       imageCloseButton = document.querySelector('.overlay__button_image'),
-      newName = overlay.querySelector('.overlay__input_form_name'),
-      newSub = overlay.querySelector('.overlay__input_form_subscription'),
+      newName = document.querySelector('.overlay__input_form_name'),
+      newSub = document.querySelector('.overlay__input_form_subscription'),
       elementTemplate = document.querySelector('#element-template').content,
       profileName = document.querySelector('.profile__name'),
       profileSub = document.querySelector('.profile__subscription'),
-      elementTitle = document.querySelectorAll('.element__title'),
-      elementImage = document.querySelectorAll('.element__picture'),
-      elementsItem = document.querySelector('.elements__items');
+      overlayAdd = document.querySelector('.overlay__add-popup'),
+      overlayEdit = document.querySelector('.overlay__edit-popup'),
+      openImagePopup = document.querySelector('.overlay__image-popup');
 
+//открытие попапа
 
-function openPopup(){
-  overlay.classList.add('overlay_active');
-  editPopup.classList.add('overlay__popup_form-edit_active');
-
+function openPopup(overlayName){
+  overlayName.classList.add('overlay_active');
 }
 
-function closePopup() {
-    overlay.classList.remove('overlay_active');
-    editPopup.classList.remove('overlay__popup_form-edit_active');
-}
+function openImage(evt){
+  openImagePopup.classList.add('overlay_active');
+  openImagePopup.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+  document.querySelector('.overlay__popup_image').classList.add('overlay__popup_image_active');
+  let overlayImage = document.querySelector('.overlay__image');
+  overlayImage.src=evt.target.src;
+  document.querySelector('.overlay__caption').textContent=evt.target.closest('.element').querySelector('.element__title').textContent;
+ };
 
-function popupIsOpened(){
-  openPopup();
+ function popupIsOpened(){
+  openPopup(overlayEdit);
   newName.value = profileName.textContent;
   newSub.value = profileSub.textContent;
 }
@@ -44,21 +39,82 @@ function submitAddElemnts(event){
   event.preventDefault();
   profileName.textContent = newName.value;
   profileSub.textContent = newSub.value;
-  closePopup();
+  closePopup(overlayEdit);
 }
 
-openButton.addEventListener('click', popupIsOpened);
-closeButtonEdit.addEventListener('click', closePopup);
-form.addEventListener('submit', submitAddElemnts);
+//закрытие
 
-//Возможно, в будущем придется удалить
+function closePopup(overlayName) {
+  overlayName.classList.remove('overlay_active'); //при закрытии блок почему-то уезжает в левый угол :(
+};
+
+function closeImage(){
+  openImagePopup.classList.remove('overlay_active');
+  document.querySelector('.overlay__popup_image').classList.remove('overlay__popup_image_active');
+};
+
+/*Клик по оверлею
 overlay.addEventListener('click', function(event){
   if (event.target === event.currentTarget){
-    closePopup();
-    closeAddPopup();
-    closeImage ()
   }
-  });
+  });*/
+
+
+
+//добавление новой карточки
+
+function addNewCard(elem){
+  list.prepend(elem);
+}
+
+const newElementName = document.querySelector('.overlay__input_element_name'),
+newElementLink = document.querySelector('.overlay__input_element_link'),
+addPopup = document.querySelector('.overlay__popup_form-add');
+
+
+function handleSubmit(event){
+event.preventDefault();
+const newEl = addCard(newElementName.value, newElementLink.value);
+addNewCard(newEl);
+document.querySelectorAll('.overlay__form')[1].reset();
+closePopup(overlayAdd);
+}
+
+function setListeners(element) {
+	element.querySelector('.element__like').addEventListener('click', addLikes);
+  element.querySelector('.element__bin').addEventListener('click', deleteCards);
+  element.querySelector('.element__picture').addEventListener('click',openImage);
+}
+
+function addLikes(evt){
+  evt.target.classList.toggle('element__like_black');
+}
+
+function deleteCards(evt){
+  evt.target.parentElement.remove();
+}
+
+//обработчики
+
+
+addButton.addEventListener('click', function(){
+  openPopup(overlayAdd);
+});
+
+closeButtonAdd.addEventListener('click', function(){
+  closePopup(overlayAdd);
+})
+
+openButton.addEventListener('click', popupIsOpened);
+closeButtonEdit.addEventListener('click',function(){
+  closePopup(overlayEdit);
+});
+form.addEventListener('submit', submitAddElemnts);
+
+addPopup.addEventListener('submit', handleSubmit);
+
+imageCloseButton.addEventListener('click', closeImage);
+
 
 //добавление исходных карточек
 
@@ -96,78 +152,21 @@ function render() {
 initialCards.forEach((item,i) =>{
   const name=initialCards[i].name;
   const link = initialCards[i].link;
-  addCard(name,link);
+  const el = addCard(name,link);
+  addCardDocElem(el);
 })
 };
 
+function addCardDocElem(elem){
+  list.append(elem);
+}
 
 function addCard(name,link){
   const element = elementTemplate.cloneNode(true);
 	element.querySelector('.element__title').textContent = name;
   element.querySelector('.element__picture').src = link;
   setListeners(element);
-  list.append(element);
+  return element;
 }
 
-
-
-function setListeners(element) {
-	element.querySelector('.element__like').addEventListener('click', addLikes);
-  element.querySelector('.element__bin').addEventListener('click', deleteCards);
-  element.querySelector('.element__picture').addEventListener('click',openImage);
-}
-
-function addLikes(evt){
-  evt.target.classList.toggle('element__like_black');
-}
-
-function deleteCards(evt){
-  evt.target.parentElement.remove();
-}
-
-function openImage(evt){
-  overlay.classList.add('overlay_active');
-  overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-  document.querySelector('.overlay__popup_image').classList.add('overlay__popup_image_active');
-  let overlayImage = document.querySelector('.overlay__image');
-  overlayImage.src=evt.target.src;
-  document.querySelector('.overlay__caption').textContent=evt.target.closest('.element').querySelector('.element__title').textContent;
- }
-
- const newElementName = document.querySelector('.overlay__input_element_name'),
- newElementLink = document.querySelector('.overlay__input_element_link'),
- addPopup = document.querySelector('.overlay__popup_form-add');
-
-
-function handleSubmit(event){
-  event.preventDefault();
-  addCard(newElementName.value, newElementLink.value);
-  document.querySelectorAll('.overlay__form')[1].reset();
-  closeAddPopup();
-}
-
-addPopup.addEventListener('submit', handleSubmit)
-
-function openAddPopup(){
-  overlay.classList.add('overlay_active');
-  addPopup.classList.add('overlay__popup_form-add_active');
-}
-
-function closeAddPopup() {
-  overlay.classList.remove('overlay_active');
-  addPopup.classList.remove('overlay__popup_form-add_active');
-
-}
-
-addButton.addEventListener('click', openAddPopup);
-closeButtonAdd.addEventListener('click', closeAddPopup)
-
-
-
-function closeImage(){
-  overlay.classList.remove('overlay_active');
-  document.querySelector('.overlay__popup_image').classList.remove('overlay__popup_image_active');
-};
-
-imageCloseButton.addEventListener('click', closeImage)
 render();
