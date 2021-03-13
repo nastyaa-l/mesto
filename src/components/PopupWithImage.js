f0477ced177aa7c63bfee6400b309f749b06630e
Anastasia <nastya.lyugaeva@bk.ru>
2021-03-13 10:46:09 +0300

    rename

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,15 +4,15 @@ import {Popup} from "./Popup.js";
 export class PopupWithImage extends Popup {
   constructor(popupSelector){
     super(popupSelector);
-    this._popopImage = this._popupElement.querySelector('.popup__image');
-    this._popupCaption = this._popupElement.querySelector('.popup__caption');
+    this._popopCardImage = this._popupElement.querySelector('.popup__image');
+    this._captionImage = this._popupElement.querySelector('.popup__caption');
   }
 
 // открытие изображение, смена картинки и описания карточки
   open(link, name){
     super.open();
-    this._popopImage.src = link;
-    this._popopImage.alt = name;
-    this._popupCaption.textContent = name;
+    this._popopCardImage.src = link;
+    this._popopCardImage.alt = name;
+    this._captionImage.textContent = name;
   }
 }
